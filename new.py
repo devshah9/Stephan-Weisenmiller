b2121bf2081570c8982271ba34478ec6b149a355
devshah9 <67046006+devshah9@users.noreply.github.com>
2022-07-13 23:29:54 +0530

    Update new.py

--- new.py
+++ new.py
@@ -19,7 +19,7 @@ from scrape_utils import scrape_function_bsc, scrape_function_eth
 from pyrogram import Client
 
 
-from utils import checkBSC, checkETH
+# from utils import checkBSC, checkETH
 
 
 # My telegram api
